981d62122c8c58133afb6b18bb3b455c5f928554
Hilal Er <zhilaler1@gmail.com>
2023-01-26 13:50:04 +0300

    hilal

--- src/main/java/com/ba/Main.java
+++ src/main/java/com/ba/Main.java
@@ -3,7 +3,11 @@ package com.ba;
 public class Main {
     public static void main(String[] args) {
 
+        int a=2;
+        int b=2;
+        System.out.println(a+b);
         System.out.println("Hello world!");
+
 
     }
 }
