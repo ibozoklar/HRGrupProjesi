cf03d4a415ed100f2a3d6fe836d2682e8b6f0e00
Emre ILGAR <emreilgar90@gmail.com>
2023-01-26 13:58:33 +0300

    emre

--- src/main/java/com/ba/Main.java
+++ src/main/java/com/ba/Main.java
@@ -8,6 +8,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("ihsan");
         System.out.println(84573086);
+        System.out.println("Jandarma Genel Komutanligi");
+
 
 
     }
